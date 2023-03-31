47cb55571a87312fc8d0bff01bd9609866087074
Nathan Lunn <nathandouglaslunn@gmail.com>
2023-03-31 08:35:46 -0400

    only trigger the finish line particle effect when the player crosses the finish line

--- Snow Boarder/Assets/Scripts/FinishLine.cs
+++ Snow Boarder/Assets/Scripts/FinishLine.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float winReloadDelay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
+            finishEffect.Play();
             Invoke("ReloadScene", winReloadDelay);
         }
     }
