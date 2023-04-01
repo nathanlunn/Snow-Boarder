74f510bd046d7a350b22286282d1956797af9888
Nathan Lunn <nathandouglaslunn@gmail.com>
2023-04-01 06:24:44 -0400

    create another sound effect, this time that gets triggered when the player loses by hitting their head on the snow

--- Snow Boarder/Assets/Scripts/CrashDetector.cs
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -7,11 +7,14 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loseReloadDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Ground")
         {
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", loseReloadDelay);
         }
     }
